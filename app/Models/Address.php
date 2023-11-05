8b7d76cd77c6ceb4c4ceb6e29fb7869344230946
emdommed <nurb8k@gmail.com>
2023-11-05 17:55:44 +0600

    tempalte

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    protected $table = 'addresses';
+//    protected $fillable = [
+//        'user_id',
+//        'address',
+//        'city',
+//        'state',
+//        'country',
+//        'pin code',
+//        'phone',
+//        'is_default',
+//    ];
 }
