3cb25cd07697d436a399b333a8eabfef0901696a
Bucklan <as1kenjebaev@gmail.com>
2023-11-19 03:00:13 +0600

    enums

---
+++ app/Enums/User/Gender.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums\User;
+
+enum Gender: string
+{
+case Male = 'Male';
+case Female = 'Female';
+case Other = 'Other';
+}
---
+++ app/Enums/User/Pronoun.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums\User;
+
+enum Pronoun: string
+{
+case he_him = 'he/him';
+case she_her = 'she/her';
+case they_them = 'they/them';
+case ze_zir = 'ze/zir';
+case xe_xem = 'xe/xem';
+case ey_em = 'ey/em';
+case ve_ver = 've/ver';
+}
